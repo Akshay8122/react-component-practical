940c1a48036ac107ebd62c23354d98e3cd602d49
akshayp-simform <akshay.p@simformsolutions.com>
2023-08-31 15:08:00 +0530

    delete unnecessary files

--- src/components/Input/Input.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Input.scss";
-
-const BaseInput = ({ placeholder, type, icon }) => {
-  return <input placeholder={placeholder} type={type} />;
-};
-
-export default BaseInput;
