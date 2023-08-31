55ff7514baf850387a206ce8d133c2f3ca11d14a
akshayp-simform <akshay.p@simformsolutions.com>
2023-08-31 15:01:37 +0530

    add theme in configProvider

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import React from "react";
 import "./App.scss";
 import "./styles/main.scss";
 import { ConfigProvider } from "antd";
+import theme from "./theme/Theme";
 import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorLink: "#4D5B7C",
-          colorPrimary: "#0069FF",
-        },
-        components: {
-          Divider: {
-            colorTextHeading: "#718096",
-            fontSize: "16px",
-          },
-          Input: {
-            colorTextPlaceholder: "#A0AEC0",
-          },
-        },
-      }}
-    >
+    <ConfigProvider theme={theme}>
       <div className="App">
         <SignIn />
       </div>
